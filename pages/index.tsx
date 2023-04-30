18ceb464db7146355cf75ee937ee06f5ef6b0f27
rostislavnagimov <rostislav.nagimov@gmail.com>
2023-04-30 15:06:35 -0700

    styles: use tailwind class insted of inline style

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,15 @@
 const HomePage = () => (
   <div>
-    <h1 style={{ textAlign: "center", marginTop: "40px"}}>Some initial text instead of 'Hello world!'</h1>
+    <h1
+      className="
+        mt-16
+        text-center
+        hover:font-bold
+        hover:underline
+      "
+    >
+      Some initial text instead of 'Hello world!'
+    </h1>
   </div>
 )
 
