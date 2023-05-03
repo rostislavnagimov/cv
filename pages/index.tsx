07c5fab6b206955157ea44746e3256bee1d7b73e
rostislavnagimov <rostislav.nagimov@gmail.com>
2023-05-03 13:30:52 -0700

    feat: use component structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,32 @@
+import React from 'react'
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Body from "@/components/Body"
+
 const HomePage = () => (
   <div>
-    <h1
-      className="
-        mt-16
-        text-center
-        bg-blue-500
-        hover:bg-red-500
-        transition
-        duration-500
-        ease-in-out
-        py-2
-        px-4
-        rounded-full
-      "
-    >
-      Some initial text instead of 'Hello world!'
-    </h1>
+    <Header />
+    <Body>
+      <>
+        <h1
+          className="
+            mt-16
+            text-center
+            bg-blue-500
+            hover:bg-red-500
+            transition
+            duration-500
+            ease-in-out
+            py-2
+            px-4
+            rounded-full
+          "
+        >
+          Some initial text instead of 'Hello world!'
+        </h1>
+      </>
+    </Body>
+    <Footer />
   </div>
 )
 
