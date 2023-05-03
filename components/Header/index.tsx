5681568501906fa83a1da1c9fd2deac20c5f29f9
rostislavnagimov <rostislav.nagimov@gmail.com>
2023-05-03 13:30:34 -0700

    feat: initiate component structure

---
+++ components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import styles from './index.module.scss'
+
+const Header = () => (
+  <div className={styles.header}>
+    <p>Header content</p>
+  </div>
+)
+
+export default Header
